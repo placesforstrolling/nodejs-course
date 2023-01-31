2cef7dddb959a11e9fbf8da3b18994c6d45da0c3
Mustafa <mustafaanvarov@mail.ru>
2023-01-31 13:27:42 +0500

    Completed 3.36

--- 3.26 Creating a Node Server/server.js
+++ 3.26 Creating a Node Server/server.js
@@ -5,23 +5,33 @@ const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
   if (url === "/") {
-    // if the url is the root
-    res.setHeader("Content-Type", "text/html");
     res.write("<html>");
-    res.write("<head><title>My First Page</title></head>");
-    res.write("<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>Send</button></form></body>");
+    res.write("<head><title>Enter Message</title><head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
     res.write("</html>");
     return res.end();
   }
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync('message.txt', 'Dummy text');
-    res.writeHead(302, {Location: '/'});
-    return res.end();
-
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
   }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
+  res.write("<head><title>My First Page</title><head>");
   res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
   res.write("</html>");
   res.end();
